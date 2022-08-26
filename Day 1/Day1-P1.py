33b2d2139556a75a32616f1508548e83aa1259cf
Douglas Titze <douglasmarcost2@gmail.com>
2022-08-26 16:49:24 -0400

    Final Day 1 Folder

---
+++ Day 1/Day1-P1.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+with open("input.txt", 'r') as f:
+    lines = f.readlines()
+
+lines = np.array(lines).astype(int)
+
+count = 0
+
+for i in range(1,len(lines)):
+    if lines[i] > lines[i-1]:
+        count +=1
+
+
+print(count)
