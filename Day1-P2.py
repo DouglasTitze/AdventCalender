14cefc74c2508023e5fa06332ec95c6411128d63
Douglas Titze <douglasmarcost2@gmail.com>
2022-08-26 16:46:17 -0400

    Finalized .py file

--- Day1-P2.py
+++ Day1-P2.py
@@ -1,4 +1,22 @@
+import numpy as np
+
 with open("input.txt", 'r') as f:
     lines = f.readlines()
 
-print(lines)
+lines = np.array(lines).astype(int)
+
+end = 4
+prev = sum(lines[0:3])
+count = 0
+
+while end <= len(lines):
+    start = end - 3
+    cur = sum(lines[start:end])
+    if cur > prev:
+        count += 1
+
+    prev = cur
+    start += 1
+    end += 1
+
+print(count)
