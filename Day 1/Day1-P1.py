4517f411f0cd92b42035a824f4165602f31feec8
Douglas Titze <douglasmarcost2@gmail.com>
2022-08-26 17:48:42 -0400

    Reformatted Files

--- Day 1/Day1-P1.py
+++ Day 1/Day1-P1.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-with open("input.txt", 'r') as f:
+with open("input.txt", "r") as f:
     lines = f.readlines()
 
 lines = np.array(lines).astype(int)
 
 count = 0
 
-for i in range(1,len(lines)):
-    if lines[i] > lines[i-1]:
-        count +=1
+for i in range(1, len(lines)):
+    if lines[i] > lines[i - 1]:
+        count += 1
 
 
 print(count)
